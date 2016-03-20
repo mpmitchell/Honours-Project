fad0ab3c67700ec8366b42f9b2bacaec1a297874
Matthew Mitchell <matt.mancer@gmail.com>
2016-03-20 22:12:06 +0000

    Attack animation

---
+++ Assets/Game/Scripts/Player/PlayerAnimator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PlayerAnimator : StateMachineBehaviour {
+
+  public PlayerController controller;
+
+	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    if (stateInfo.IsTag("Attack")) {
+      controller.attacking = false;
+    }
+	}
+}
--- Assets/Game/Scripts/Player/PlayerController.cs
+++ Assets/Game/Scripts/Player/PlayerController.cs
@@ -5,12 +5,9 @@ public class PlayerController : MonoBehaviour {
   [Range(1, 100)] [SerializeField] float speed;
   [Range(1, 100)] [SerializeField] float runningSpeed;
 
-  bool running;
-  float dx;
-  float dy;
-
   Animator animator;
   int moving = 0;
+  [HideInInspector] public bool attacking = false;
 
   static int wallLayerMask;
   static Vector2 colliderExtents;
@@ -21,11 +18,12 @@ public class PlayerController : MonoBehaviour {
     animator = GetComponent<Animator>();
     wallLayerMask = LayerMask.GetMask("Wall");
     colliderExtents = GetComponent<BoxCollider2D>().bounds.extents;
+    animator.GetBehaviour<PlayerAnimator>().controller = this;
   }
 
   void Update() {
-    dx = Input.GetAxis("Horizontal");
-    dy = Input.GetAxis("Vertical");
+    float dx = Input.GetAxis("Horizontal");
+    float dy = Input.GetAxis("Vertical");
     bool running = Input.GetButton("Run");
 
     if (running) {
@@ -36,10 +34,20 @@ public class PlayerController : MonoBehaviour {
       dy *= speed * Time.deltaTime;
     }
 
+    if (attacking) {
+      dx = 0.0f;
+      dy = 0.0f;
+    }
+
     CollisionCheck(ref dx, ref dy);
     Animate(dx, dy);
 
     transform.Translate(new Vector3(dx, dy, 0.0f));
+
+    if (!attacking && Input.GetButtonDown("Attack")) {
+      animator.SetTrigger("Attack");
+      attacking = true;
+    }
   }
 
   void CollisionCheck(ref float dx, ref float dy) {
@@ -48,59 +56,59 @@ public class PlayerController : MonoBehaviour {
     float bottom = transform.position.y + dy - colliderExtents.y;
     float top =    transform.position.y + dy + colliderExtents.y;
 
-    if (dx < 0) {
+    if (dx < 0.0f) {
       RaycastHit2D hitBottom = Physics2D.Raycast(new Vector2(left, bottom), Vector2.left, BOX_CAST_DISTANCE, wallLayerMask);
       RaycastHit2D hitTop = Physics2D.Raycast(new Vector2(left, top), Vector2.left, BOX_CAST_DISTANCE, wallLayerMask);
       if (hitBottom.collider != null) {
-        dx = 0;
+        dx = 0.0f;
         hitBottom.collider.SendMessage("PushLeft", SendMessageOptions.DontRequireReceiver);
       } else if (hitTop.collider != null) {
-        dx = 0;
+        dx = 0.0f;
         hitTop.collider.SendMessage("PushLeft", SendMessageOptions.DontRequireReceiver);
       }
-    } else if (dx > 0) {
+    } else if (dx > 0.0f) {
       RaycastHit2D hitBottom = Physics2D.Raycast(new Vector2(right, bottom), Vector2.right, BOX_CAST_DISTANCE, wallLayerMask);
       RaycastHit2D hitTop = Physics2D.Raycast(new Vector2(right, top), Vector2.right, BOX_CAST_DISTANCE, wallLayerMask);
       if (hitBottom.collider != null) {
-        dx = 0;
+        dx = 0.0f;
         hitBottom.collider.SendMessage("PushRight", SendMessageOptions.DontRequireReceiver);
       } else if (hitTop.collider != null) {
-        dx = 0;
+        dx = 0.0f;
         hitTop.collider.SendMessage("PushRight", SendMessageOptions.DontRequireReceiver);
       }
     }
 
-    if (dy < 0) {
+    if (dy < 0.0f) {
       RaycastHit2D hitLeft = Physics2D.Raycast(new Vector2(left, bottom), Vector2.down, BOX_CAST_DISTANCE, wallLayerMask);
       RaycastHit2D hitRight = Physics2D.Raycast(new Vector2(right, bottom), Vector2.down, BOX_CAST_DISTANCE, wallLayerMask);
       if (hitLeft.collider != null) {
-        dy = 0;
+        dy = 0.0f;
         hitLeft.collider.SendMessage("PushDown", SendMessageOptions.DontRequireReceiver);
       } else if (hitRight.collider != null) {
-        dy = 0;
+        dy = 0.0f;
         hitRight.collider.SendMessage("PushDown", SendMessageOptions.DontRequireReceiver);
       }
-    } else if (dy > 0) {
+    } else if (dy > 0.0f) {
       RaycastHit2D hitLeft = Physics2D.Raycast(new Vector2(left, top), Vector2.up, BOX_CAST_DISTANCE, wallLayerMask);
       RaycastHit2D hitRight = Physics2D.Raycast(new Vector2(right, top), Vector2.up, BOX_CAST_DISTANCE, wallLayerMask);
       if (hitLeft.collider != null) {
-        dy = 0;
+        dy = 0.0f;
         hitLeft.collider.SendMessage("PushUp", SendMessageOptions.DontRequireReceiver);
       } else if (hitRight.collider != null) {
-        dy = 0;
+        dy = 0.0f;
         hitRight.collider.SendMessage("PushUp", SendMessageOptions.DontRequireReceiver);
       }
     }
   }
 
   void Animate(float dx, float dy) {
-    if (dx < 0) {
+    if (dx < 0.0f) {
       animator.SetInteger("Direction", (int)Direction.Left);
       if (moving != (int)Direction.Left) {
         animator.SetTrigger("StartedMoving");
         moving = (int)Direction.Left;
       }
-    } else if (dx > 0) {
+    } else if (dx > 0.0f) {
       animator.SetInteger("Direction", (int)Direction.Right);
       if (moving != (int)Direction.Right) {
         animator.SetTrigger("StartedMoving");
@@ -108,13 +116,13 @@ public class PlayerController : MonoBehaviour {
       }
     }
 
-    if (dy < 0) {
+    if (dy < 0.0f) {
       animator.SetInteger("Direction", (int)Direction.Down);
       if (moving != (int)Direction.Down) {
         animator.SetTrigger("StartedMoving");
         moving = (int)Direction.Down;
       }
-    } else if (dy > 0) {
+    } else if (dy > 0.0f) {
       animator.SetInteger("Direction", (int)Direction.Up);
       if (moving != (int)Direction.Up) {
         animator.SetTrigger("StartedMoving");
@@ -122,7 +130,7 @@ public class PlayerController : MonoBehaviour {
       }
     }
 
-    if (dx == 0 && dy == 0) {
+    if (dx == 0.0f && dy == 0.0f) {
       if (moving != 0) {
         animator.SetTrigger("StoppedMoving");
         moving = 0;
