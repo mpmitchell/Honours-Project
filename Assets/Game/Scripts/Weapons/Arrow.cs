94f37a4989a2bc9a21b8ca419c95e5cdb49c504a
Matthew Mitchell <matt.mancer@gmail.com>
2016-03-21 11:28:49 +0000

    Arrow spawning, fixed some minor bugs with the animator

---
+++ Assets/Game/Scripts/Weapons/Arrow.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Arrow : MonoBehaviour {
+
+  Direction direction;
+
+  void Update() {
+    //
+  }
+
+  public void Activate(Vector3 origin, Direction direction) {
+    gameObject.SetActive(true);
+    transform.position = origin;
+
+    this.direction = direction;
+
+    switch (direction) {
+      case Direction.Left: {
+        transform.eulerAngles = new Vector3(0.0f, 0.0f, 315.0f);
+        break;
+      }
+
+      case Direction.Right: {
+        transform.eulerAngles = new Vector3(0.0f, 0.0f, 135.0f);
+        break;
+      }
+
+      case Direction.Down: {
+        transform.eulerAngles = new Vector3(0.0f, 0.0f, 45.0f);
+        break;
+      }
+
+      case Direction.Up: {
+        transform.eulerAngles = new Vector3(0.0f, 0.0f, 225.0f);
+        break;
+      }
+    }
+  }
+}
---
+++ Assets/Game/Scripts/Weapons/ArrowPool.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ArrowPool : MonoBehaviour {
+  [SerializeField] GameObject arrowPrefab;
+  [SerializeField] int numberOfArrows;
+
+  Stack<GameObject> arrowPool = new Stack<GameObject>();
+
+  void Awake() {
+    for (int i = 0; i < numberOfArrows; i++) {
+      GameObject arrow = Instantiate(arrowPrefab) as GameObject;
+      arrow.SetActive(false);
+      arrowPool.Push(arrow);
+    }
+  }
+
+  public GameObject GetArrow() {
+    if (arrowPool.Count != 0) {
+      return arrowPool.Pop();
+    } else {
+      return null;
+    }
+  }
+}
