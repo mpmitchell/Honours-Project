ee2a9f5aaa257626192fa45eff8bef07a52df906
Matthew Mitchell <matt.mancer@gmail.com>
2016-03-23 17:25:24 +0000

    Objects breakable by other means of damage

--- Assets/Game/Scripts/Weapons/Damage.cs
+++ Assets/Game/Scripts/Weapons/Damage.cs
@@ -3,11 +3,14 @@
 public class Damage : MonoBehaviour {
 
   [SerializeField] [Range(1, 10)] int damage;
+  [SerializeField] bool canBreak;
   [SerializeField] LayerMask targetLayers;
 
   void OnTriggerEnter2D(Collider2D collider) {
     if (((1 << collider.gameObject.layer) & targetLayers) != 0) {
       collider.SendMessage("Damage", damage, SendMessageOptions.DontRequireReceiver);
+    } else if (canBreak && collider.tag == "Breakable") {
+      collider.SendMessage("Break");
     }
   }
 }
