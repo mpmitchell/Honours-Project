7c782ed85c8667e034ac91d0dfc43e68c9087124
Matthew Mitchell <matt.mancer@gmail.com>
2016-03-23 19:05:18 +0000

    Room transitions

---
+++ Assets/Game/Scripts/Environment/Room.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Room : MonoBehaviour {
+
+  [SerializeField] bool startingRoom;
+
+  public static Room currentRoom = null;
+  public static Room entering = null;
+
+  void Awake() {
+    if (startingRoom) {
+      currentRoom = this;
+    }
+  }
+}
