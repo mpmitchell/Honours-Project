6e1f8e82a2c07572e0fe7a43ffaee309c4bb781d
Matthew Mitchell <matt.mancer@gmail.com>
2016-03-23 10:45:50 +0000

    Bat behaviour

---
+++ Assets/Game/Scripts/Enemies/Bat.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Bat : MonoBehaviour {
+
+  [SerializeField] [Range(1, 10)] int speed;
+  [SerializeField] LayerMask wallLayerMask;
+  [SerializeField] [Range(1, 10)] int projectionFactor;
+
+  Vector3 direction;
+
+  void Awake() {
+    direction = Random.insideUnitCircle.normalized;
+  }
+
+  void Update() {
+    Vector3 projectedPosition = transform.position + direction * speed * Time.deltaTime * projectionFactor;
+    Vector3 deltaPosition = projectedPosition - transform.position;
+    RaycastHit2D hit = Physics2D.Raycast(transform.position, deltaPosition, deltaPosition.magnitude, wallLayerMask);
+    Debug.DrawLine(transform.position, projectedPosition);
+
+    if (hit.collider != null) {
+      direction = Random.insideUnitCircle.normalized;
+      projectedPosition = transform.position + direction * speed * Time.deltaTime * projectionFactor;
+      deltaPosition = projectedPosition - transform.position;
+      hit = Physics2D.Raycast(transform.position, deltaPosition, deltaPosition.magnitude, wallLayerMask);
+      Debug.DrawLine(transform.position, projectedPosition);
+    }
+
+    transform.Translate(direction * speed * Time.deltaTime);
+  }
+}
