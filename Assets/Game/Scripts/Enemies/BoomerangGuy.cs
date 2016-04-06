13841b87ad778d99196df01d40e662c59628c66a
Matthew Mitchell <matt.mancer@gmail.com>
2016-04-06 10:02:54 +0100

    Boomerang Guy

---
+++ Assets/Game/Scripts/Enemies/BoomerangGuy.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class BoomerangGuy : MovingEntity {
+
+  [SerializeField] [Range(1, 10)] int speed;
+  [SerializeField] [Range(1, 100)] int changeChance;
+  [SerializeField] [Range(1, 100)] int fireChance;
+
+  float distance;
+  bool attacking = false;
+
+  void Start() {
+    SelectDirection();
+  }
+
+  void Update() {
+    if (!attacking) {
+      Vector3 translation = GetDirectionVector() * speed * Time.deltaTime;
+      CollisionCheck(ref translation.x, ref translation.y);
+      distance += translation.magnitude;
+
+      if (translation == Vector3.zero) {
+        SelectDirection();
+      } else if (distance >= 1.0f) {
+        distance = 0.0f;
+
+        if (Random.value <= 1.0f / changeChance) {
+          SelectDirection();
+        } else if (Random.value <= 1.0f / fireChance) {
+          SetMoving(0.0f, 0.0f);
+          SendMessage("FireProjectile", direction);
+          attacking = true;
+        }
+      } else {
+        SetMoving(translation.x, translation.y);
+        transform.Translate(translation);
+      }
+    }
+  }
+
+  void ProjectileReturned() {
+    attacking = false;
+    SelectDirection();
+  }
+
+  void SelectDirection() {
+    distance = 0.0f;
+
+    if (direction == Direction.Left || direction == Direction.Right) {
+      direction = Random.value > 0.5f ? Direction.Up : Direction.Down;
+    } else {
+      direction = Random.value > 0.5f ? Direction.Left : Direction.Right;
+    }
+
+    UpdateDirection();
+  }
+}
---
+++ Assets/Game/Scripts/Weapons/Projectile.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+  [SerializeField] [Range(1, 10)] int speed;
+  [SerializeField] bool boomerang;
+  [SerializeField] [Range(1, 10)] int distance;
+
+  [HideInInspector] public ProjectileSpawner spawner;
+
+  Vector3 direction;
+
+  float distanceTraveled = 0.0f;
+  bool boomeranged = false;
+
+  void Update() {
+    distanceTraveled += speed * Time.deltaTime;
+
+    if (boomerang && distanceTraveled >= distance) {
+      if (!boomeranged) {
+        direction = -direction;
+        distanceTraveled = 0.0f;
+        boomeranged = true;
+      } else {
+        spawner.ReturnProjectile(gameObject);
+      }
+    } else {
+      transform.Translate(direction * speed * Time.deltaTime, Space.World);
+    }
+  }
+
+  void OnTriggerEnter2D(Collider2D collider) {
+    if (boomerang && !boomeranged) {
+      direction = -direction;
+      distanceTraveled = distance - distanceTraveled;
+      boomeranged = true;
+    } else {
+      spawner.ReturnProjectile(gameObject);
+    }
+  }
+
+  public void Activate(Vector3 origin, Direction direction) {
+    gameObject.SetActive(true);
+    transform.position = origin;
+    distanceTraveled = 0.0f;
+    boomeranged = false;
+
+    switch (direction) {
+      case Direction.Left: {
+        transform.eulerAngles = new Vector3(0.0f, 0.0f, 315.0f);
+        this.direction = Vector3.left;
+        break;
+      }
+
+      case Direction.Right: {
+        transform.eulerAngles = new Vector3(0.0f, 0.0f, 135.0f);
+        this.direction = Vector3.right;
+        break;
+      }
+
+      case Direction.Down: {
+        transform.eulerAngles = new Vector3(0.0f, 0.0f, 45.0f);
+        this.direction = Vector3.down;
+        break;
+      }
+
+      case Direction.Up: {
+        transform.eulerAngles = new Vector3(0.0f, 0.0f, 225.0f);
+        this.direction = Vector3.up;
+        break;
+      }
+    }
+  }
+}
