a2311b261c8f8c052a18024578621927e31c6daf
Matthew Mitchell <matt.mancer@gmail.com>
2016-04-06 23:36:26 +0100

    Doorways fixed

--- Assets/Game/Scripts/Environment/Doorway.cs
+++ Assets/Game/Scripts/Environment/Doorway.cs
@@ -5,30 +5,21 @@ public class Doorway : MonoBehaviour {
 
   [SerializeField] Room room;
 
-  static GameObject previousDoorway = null;
-
   void OnTriggerEnter2D(Collider2D collider) {
     if (room != Room.currentRoom) {
       // Move Camera & player
       Camera.main.transform.position = new Vector3(room.transform.position.x, room.transform.position.y, Camera.main.transform.position.z);
       PlayerController.player.transform.position = transform.position;
 
-      // Block previousDoorway
-      previousDoorway.layer = LayerMask.NameToLayer("Obstacle");
-
       Room.currentRoom.Exit();
       room.Enter();
+
       Room.currentRoom = room;
-    } else {
-      previousDoorway = gameObject;
     }
   }
 
   void OnTriggerExit2D(Collider2D collider) {
-    if (room == Room.currentRoom && previousDoorway) {
-      previousDoorway.layer = LayerMask.NameToLayer("Doorway");
-      previousDoorway = null;
-
+    if (room == Room.currentRoom) {
       // Trigger gate spawns
       room.LockGates();
     }
