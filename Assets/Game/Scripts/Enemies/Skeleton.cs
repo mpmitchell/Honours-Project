57d7ec7c9f76382dffd3073b2e1ed409ea8501f9
Matthew Mitchell <matt.mancer@gmail.com>
2016-04-06 15:10:51 +0100

    Update Skeleton to match Boomerang Guy

---
+++ Assets/Game/Scripts/Enemies/Skeleton.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class Skeleton : MovingEntity {
+
+  [SerializeField] [Range(1, 10)] int speed;
+  [SerializeField] [Range(1, 10)] int changeChance;
+
+  float distance;
+
+  void Start() {
+    SelectDirection();
+  }
+
+  void Update() {
+    Vector3 translation = GetDirectionVector() * speed * Time.deltaTime;
+    CollisionCheck(ref translation.x, ref translation.y);
+    distance += translation.magnitude;
+
+    if (translation == Vector3.zero) {
+      SelectDirection();
+    } else if (distance >= 1.0f) {
+      distance = 0.0f;
+
+      if (Random.value <= 1.0f / changeChance) {
+        SelectDirection();
+      }
+    } else {
+      SetMoving(translation.x, translation.y);
+      transform.Translate(translation);
+    }
+  }
+
+  void SelectDirection() {
+    if (Random.value <= 1.0f / changeChance) {
+      distance = 0.0f;
+
+      if (direction == Direction.Left || direction == Direction.Right) {
+        direction = Random.value > 0.5f ? Direction.Up : Direction.Down;
+      } else {
+        direction = Random.value > 0.5f ? Direction.Left : Direction.Right;
+      }
+
+      UpdateDirection();
+    }
+  }
+}
